e4b77a357ba7404d0aebb6060d907f54f5b12608
Rudolfo Borges Oliveira <rudolfoborges@Rudolfos-MacBook-Pro.local>
2014-04-22 01:56:02 -0300

    AngularJS and Ruby on Rails App Car List

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,8 @@
 var app = angular.module('app', ['ngRoute', 'ngResource']);
 
-app.config(function($routeProvider){
+app.config(function($routeProvider, $locationProvider){
 	$routeProvider.
-	when('/', {controller: 'CarController', templateUrl: '/templates/car/main.html'});
-
+		when('/', {controller: 'CarController', templateUrl: '/templates/car/main.html'}).
+		when('/teste', {controller: 'CarController', templateUrl: '/templates/car/teste.html'});
+	$locationProvider.html5Mode(true);
 });
